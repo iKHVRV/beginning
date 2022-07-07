5333b110b88d713edc768a10d5fff8ccefe7cfc6
Dmitriy <104030338+iKHVRV@users.noreply.github.com>
2022-07-07 23:30:26 +0300

    Beta Update v2.2
    Bug fix
    Adding a pop-up window

--- js/main.js
+++ js/main.js
@@ -23,6 +23,20 @@ function sideBar() {
     }
 }
 
+const keySup = document.getElementById('keySupport');
+const windowSup = document.getElementById('windowSup');
+const closeWin = document.getElementById('closeWin');
+
+keySup.onclick = windowClk;
+closeWin.onclick = closeW;
+
+function windowClk() {
+    windowSup.style.display = 'block';
+}
+
+function closeW() {
+    windowSup.style.display = 'none';
+}
 
 let counterL = 0;
 let counterC = 0;
